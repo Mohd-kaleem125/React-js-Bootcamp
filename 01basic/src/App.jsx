fe2e65ba95f22ec6e76223255aba33ea14d8797a
Mohd-kaleem125 <kaleemhussainans786@gmail.com>
2026-02-04 12:18:03 +0530

    add react project files

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return(
+    <div style={{textAlign: 'center', marginTop: 200}}>
+      Hello, start react series..
+    </div>
+  )
+
+ 
+}
+
+export default App
