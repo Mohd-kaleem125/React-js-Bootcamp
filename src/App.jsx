26afadd0cdd6779aff2704322a4b2c07f4828ba4
Mohd-kaleem125 <kaleemhussainans786@gmail.com>
2026-02-06 19:13:27 +0530

    push my project o2 on github

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ const [counter, setCounter]  = useState(15);
+
+
+ const addValue = () => {
+  console.log("Clicked", counter);
+ // setCounter(counter+1); These four lines increase value only one times that is equal to 16
+ // setCounter(counter+1);
+ // setCounter(counter+1);
+ // setCounter(counter+1);
+ // setCounter(counter+1);
+
+ // Handle This situation by using call back prevCounter
+
+ setCounter(prevCounter => prevCounter +1);
+ setCounter(prevCounter => prevCounter +1);
+ setCounter(prevCounter => prevCounter +1);
+ setCounter(prevCounter => prevCounter +1);
+ }
+
+ const removeValue = () => {
+  setCounter(counter - 1);
+ }
+
+
+
+
+  return (
+  <div>
+   
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter} </h2>
+
+    <button onClick={addValue}>Add Value</button>
+    <button onClick={removeValue}>Remove Value {counter}</button>
+    <p>footer: {counter}</p>
+  </div>
+ 
+  )
+}
+
+export default App
